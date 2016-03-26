44c2c56af78b14f1db23ffaa51f565616fde3357
Zhouchao <wozhouchao@hotmail.com>
2016-03-26 00:12:18 -0700

    adsjlkfhbask jyewq	oih`21

--- P3.js
+++ P3.js
@@ -237,12 +237,14 @@ function addRob1(){
 
 var components = [];  // body, head, lhand, rhand, lleg, rleg
 var material = new THREE.MeshBasicMaterial( {color: 'green'} );
-
+var rob2_init;
 function addRob2(){
    
   var body_geo = new THREE.CylinderGeometry( 2, 2, 4, 32 );
   var body = new THREE.Mesh(body_geo,material);
+  rob2_init = new THREE.Mesh(body_geo,material);
   scene.add(body);
+
   body.position.set(10,4,0);
   components.push(body);
 
@@ -359,6 +361,13 @@ function rob2_defend(){
   components[3].rotateZ(-Math.PI/120);
   rob2_wall.scale.y+=0.02;
 }
+
+var rob2_frame = 0;
+function rob2_attack(){
+  if(rob2_frame<=30){components[0].rotateZ(Math.PI/60); rob2_frame++;}
+  else{components[0].translateY(1);}
+
+}
 function updateBody() {
   switch(true)
   {
@@ -388,17 +397,26 @@ function updateBody() {
       if (time > time_end){
         p = p1;
         animate = false;
-        resetBody();
+        break;
+      }
+
+      p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
+      rob2_attack(p);
+
+      
+      break
+
+      case(key == 2 && animate):
+      var time = clock.getElapsedTime(); // t seconds passed since the clock started.
+
+      if (time > time_end){
+        p = p1;
+        animate = false;
         break;
       }
 
       p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
 
-
-
-      for(var i=0;i<6;i++){
-              components[i].translateX(0.1);
-      }
 
       
       break
@@ -409,7 +427,6 @@ function updateBody() {
       if (time > time_end){
         p = p1;
         animate = false;
-        resetBody();
         break;
       }
 
@@ -424,7 +441,6 @@ function updateBody() {
       if (time > time_end){
         p = p1;
         animate = false;
-
         break;
       }
 
@@ -477,12 +493,13 @@ function removeText(){scene.remove(texts[key]);}
 function addText(){scene.add(texts[key]);}
 
 function resetBody(){
-  for(var i=0;i<6;i++){
-    components[i].rotation.z=0;}
   scene.remove(rob2_wall);
   rob2_wall.scale.y = 1;
   is_wall = 0;
   components_a[1].rotation.y = 0;
+  components[0].geometry = rob2_init.geometry;
+  components[0].position.set(10,4,0);
+  rob2_frame = 0;
 }
 
 function takeAction(){
